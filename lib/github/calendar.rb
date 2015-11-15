c84c24de53d0a2db5f5fd4f40f41bc3af9d61257
elifoster <elifosterwy@gmail.com>
2015-11-15 13:02:22 -0800

    Handle HTTPErrors with nil

--- lib/github/calendar.rb
+++ lib/github/calendar.rb
@@ -10,8 +10,10 @@ module GitHub
     # Gets the total number of contributions for the past year.
     # @param user [String] The username to get this data for.
     # @return [Int] An integer value of their total contributions this year.
+    # @return [Nil] If the user cannot be found.
     def get_total_year(user)
       source = get_page_source(user)
+      return nil if source.nil?
       string = source.css('span.contrib-number')[0].text
       string.to_i_separated
     end
@@ -19,8 +21,10 @@ module GitHub
     # Gets the longest contribution streak (in days) that the user has had.
     # @param user [String] See #get_total_year
     # @return [Int] The user's longest contribution streak in days.
+    # @return [Nil] See #get_total_year
     def get_longest_streak(user)
       source = get_page_source(user)
+      return nil if source.nil?
       string = source.css('span.contrib-number')[1].text
       string.to_i_separated
     end
@@ -28,8 +32,10 @@ module GitHub
     # Gets the current contribution streak (in days) that the user is in.
     # @param user [String] See #get_total_year
     # @return [Int] The user's current contribution streak in days.
+    # @return [Nil] See #get_total_year
     def get_current_streak(user)
       source = get_page_source(user)
+      return nil if source.nil?
       string = source.css('span.contrib-number')[2].text
       string.to_i_separated
     end
@@ -38,8 +44,10 @@ module GitHub
     # @param user [String] See #get_total_year
     # @return [Hash] How many contributions they have done each week. Example:
     # { 0: 0, 1: 8, etc. }
+    # @return [Nil] See #get_total_year
     def get_weekly(user)
       weeks = get_calendar(user)
+      return nil if weeks.nil?
       ret = {}
       count = 0
       weeks[1..-1].each do |k|
@@ -58,8 +66,10 @@ module GitHub
     # @param user [String] See #get_total_year
     # @return [Hash] How many contributions they have performed each day. See
     # #get_weekly
+    # @return [Nil] See #get_total_year
     def get_daily(user)
       weeks = get_calendar(user)
+      return nil if weeks.nil?
       ret = {}
       count = 0
       weeks[1..-1].each do |k|
@@ -77,8 +87,10 @@ module GitHub
     # @return [Hash] How many contributions they have performed each month.
     # Months are listed as their string integers, e.g., 01 is January and
     # 12 is December.
+    # @return [Nil] See #get_total_year
     def get_monthly(user)
       weeks = get_calendar(user)
+      return nil if weeks.nil?
       ret = {
         '01' => 0,
         '02' => 0,
@@ -106,22 +118,31 @@ module GitHub
     # Gets the average weekly number of contributions by the user.
     # @param user [String] See #get_total_year
     # @return [Int] The average number of contributions.
+    # @return [Nil] See #get_total_year
     def get_average_week(user)
-      get_average(get_weekly(user))
+      weekly = get_weekly(user)
+      return nil if weekly.nil?
+      get_average(weekly)
     end
 
     # Gets the average daily number of contributions by the user.
     # @param user [String] See #get_total_year
     # @return [Int] See #get_average_week
+    # @return [Nil] See #get_total_year
     def get_average_day(user)
-      get_average(get_daily(user))
+      daily = get_daily(user)
+      return nil if daily.nil?
+      get_average(daily)
     end
 
     # Gets the average monthly number of contributions by the user.
     # @param user [String] See #get_total_year
     # @return [Int] See #get_average_week
+    # @return [Nil] See #get_total_year
     def get_average_month(user)
-      get_average(get_monthly(user))
+      monthly = get_monthly(user)
+      return nil if monthly.nil?
+      get_average(monthly)
     end
 
     private
@@ -129,8 +150,13 @@ module GitHub
     # Gets the parsed HTML source for the user profile.
     # @param user [String] See #get_total_year
     # @return [Nokogiri::HTML::Document] The parsed HTML for the user page
+    # @return [Nil] If OpenURI throws an HTTPError.
     def get_page_source(user)
-      Nokogiri::HTML(open("https://github.com/#{user}"), &:noblanks)
+      begin
+        Nokogiri::HTML(open("https://github.com/#{user}"), &:noblanks)
+      rescue OpenURI::HTTPError
+        return nil
+      end
     end
 
     # Gets the parsed calendar HTML source for the user profile.
@@ -138,8 +164,10 @@ module GitHub
     # @return [Nokogiri::XML::NodeSet] The NodeSet for all the g's in the
     # calendar. Consider this as an array of all the weeks. In iteration,
     # you will probably want to skip the first, as it is the total yearly.
+    # @return [Nil] See #get_total_year
     def get_calendar(user)
       source = get_page_source(user)
+      return nil if source.nil?
       source.css('svg.js-calendar-graph-svg g')
     end
 
